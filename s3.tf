dd90e79b2fd65b51a49aa83d754f7b32389bec98
Satyajit <ahana@Sattu--Mittu.local>
2022-02-18 02:22:07 +0530

    Added code for S3 & vpc basic template

---
+++ s3.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "onebucket" {
+   bucket = "sattumittu-s3-with-terraform"
+   acl = "private"
+   versioning {
+      enabled = true
+   }
+   tags = {
+     Name = "Bucket1"
+     Environment = "Test"
+   }
+}
